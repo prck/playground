70b2f95a140adf9b046ec15102b452698ff7c79e
Pierrick <pierrick.leduc@opcalia.com>
2018-04-04 10:52:36 +0200

    Express, heroku, mdi, Vuetify

---
+++ src/components/header/header.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <app-aside :drawer="drawer"></app-aside>
+    <v-toolbar color="blue darken-3" dark app :clipped-left="$vuetify.breakpoint.lgAndUp" fixed>
+      <v-toolbar-title style="width: 300px" class="ml-0 pl-3">
+        <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+        <span class="hidden-sm-and-down">Google Contacts</span>
+      </v-toolbar-title>
+      <v-text-field flat solo-inverted prepend-icon="search" label="Search" class="hidden-sm-and-down"></v-text-field>
+      <v-spacer></v-spacer>
+      <v-btn icon>
+        <v-icon>apps</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>notifications</v-icon>
+      </v-btn>
+      <v-btn icon large>
+        <v-avatar size="32px" tile>
+          <img src="https://vuetifyjs.com/static/doc-images/logo.svg" alt="Vuetify">
+        </v-avatar>
+      </v-btn>
+    </v-toolbar>
+  </div>
+</template>
+
+<script>
+import aside from '../aside/aside.vue';
+export default {
+  data: () => {
+    return {
+      drawer: true
+    }
+  },
+  components: {
+    'app-aside': aside
+  }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -2,9 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.css'
+
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.css'
 
 Vue.config.productionTip = false
 
+Vue.use(VueMaterial)
+Vue.use(Vuetify)
 new Vue({
   router,
   store,
