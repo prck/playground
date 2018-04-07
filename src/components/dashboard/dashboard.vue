8be9344af48d8e24d6c3b015d7a67c2c3049a90a
Pierrick <tchontk@gmail.com>
2018-04-07 16:12:30 +0200

    IHM

--- src/components/dashboard/dashboard.vue
+++ src/components/dashboard/dashboard.vue
@@ -1,12 +1,15 @@
 <template>
   <div>
+    Dashboard
     <app-board/>
+    <v-btn fab bottom right color="blue" dark fixed @click.stop="dialog = !dialog">
+      <v-icon>add</v-icon>
+    </v-btn>
   </div>
 </template>
 
-
 <script>
-import board from "../board/board.vue";
+import board from "./Board.vue";
 export default {
   components: {
     "app-board": board
