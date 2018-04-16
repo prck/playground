ff3d746a2534ebab4f6904947542cc9ea29db5a4
Pierrick <pierrick.leduc@opcalia.com>
2018-04-16 14:01:02 +0200

    New Header (Vuetify -> Bootstrap)

--- src/components/layout/header.vue
+++ src/components/layout/header.vue
@@ -1,38 +1,20 @@
 <template>
-  <div>
-    <app-aside :drawer="drawer" />
-    <v-toolbar :clipped-left="$vuetify.breakpoint.lgAndUp" color="blue darken-3" fixed app dark>
-      <v-toolbar-title style="width: 300px" class="ml-0 pl-3">
-        <v-toolbar-side-icon @click.stop="drawer = !drawer" />
-        <span class="hidden-sm-and-down">Kanban</span>
-      </v-toolbar-title>
-      <!-- <v-text-field flat solo-inverted prepend-icon="search" label="Search" class="hidden-sm-and-down"></v-text-field> -->
-      <v-spacer />
-      <v-btn icon>
-        <v-icon>apps</v-icon>
-      </v-btn>
-      <v-btn icon>
-        <v-icon>notifications</v-icon>
-      </v-btn>
-      <v-btn icon large>
-        <v-avatar size="32px" tile>
-          <img src="https://vuetifyjs.com/static/doc-images/logo.svg" alt="Vuetify">
-        </v-avatar>
-      </v-btn>
-    </v-toolbar>
-  </div>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon" />
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="/">Home
+            <span class="sr-only">(current)</span>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/board">Board</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
 </template>
-
-<script>
-import aside from "./Aside.vue";
-export default {
-  components: {
-    "app-aside": aside
-  },
-  data: () => {
-    return {
-      drawer: false
-    };
-  }
-};
-</script>
