5f691a37e47fad427222137fd1b2276147fcd45c
Pierrick <pierrick.leduc@opcalia.com>
2018-04-16 14:00:33 +0200

    Modification route / store

--- src/routes.js
+++ src/routes.js
@@ -2,38 +2,27 @@ import Layout from './components/layout/Header.vue';
 import Home from './components/Home.vue';
 import Board from './components/board/Board.vue';
 import Dashboard from './components/dashboard/Dashboard.vue';
-import model from './components/Model.vue';
-
-// import Stocks from './components/stock/Stocks.vue';
-
-// import Portfolio from './components/portfolio/Portfolio.vue';
 
 export const routes = [{
   path: '',
   name: 'home',
   components: {
     default: Home,
-    'header-top': Layout,
+    'header': Layout,
   }
 }, {
   path: '/dashboard',
   name: 'dashboard',
   components: {
     default: Dashboard,
-    'header-top': Layout,
+    'header': Layout,
   }
 }, {
   path: '/board',
   name: 'board',
   components: {
     default: Board,
-    'header-top': Layout,
-  }
-}, {
-  path: '/model',
-  name: 'model',
-  components: {
-    default: model
+    'header': Layout,
   }
 }, {
   path: '*',
--- src/store/store.js
+++ src/store/store.js
@@ -1,16 +1,13 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import boards from './modules/boards'
-import lists from './modules/lists'
-import cards from './modules/cards'
-import comments from './modules/comments'
 // import * as actions from './actions'
 
 Vue.use(Vuex);
 
 export const store = new Vuex.Store({
   // actions,
-  modules: { boards, lists, cards, comments }
+  modules: { boards }
 })
 
 // export default new Vuex.Store({
